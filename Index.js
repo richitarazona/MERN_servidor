898867efc2dc8e9e08975349eeb5204b4c61d2ba
richi <ricardomoronweb@gmail.com>
2020-04-08 17:02:08 +0200

    256

--- Index.js
+++ Index.js
@@ -18,6 +18,7 @@ const PORT = process.env.PORT || 4000;
 //Importamos las rutas
 app.use('/api/usuarios', require('./routes/usuarios'));
 app.use('/api/auth', require('./routes/auth'));
+app.use('/api/proyectos', require('./routes/proyectos'));
 
 
 // arrancamos la app
---
+++ routes/proyectos.js
@@ -0,0 +1,12 @@
+//Rutas para crear proyectos
+const express = require('express');
+const router = express.Router();
+const proyectoController = require('../controllers/proyectocontroller');
+
+// Crea proyectos
+// api/proyectos
+router.post('/', 
+    proyectoController.crearProyecto
+);
+
+module.exports = router;
